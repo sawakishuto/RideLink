5f34688a8a2a25d713a119306e814755c9786ecc
Sawaki Shuto <swkshuto0208@icloud.com>
2024-03-27 00:51:27 +0900

    Project作成, Firebase,coreData設定

---
+++ RideLink/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RideLink
+//
+//  Created by 澤木柊斗 on 2024/03/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
