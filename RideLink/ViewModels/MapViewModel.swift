875b33e678f8f59ac93f4d8cd7b2a8ca0e1db0b1
Sawaki Shuto <swkshuto0208@icloud.com>
2024-04-26 23:05:06 +0900

    追加: MapViewに必要なデリゲートメソッド追加

---
+++ RideLink/ViewModels/MapViewModel.swift
@@ -0,0 +1,31 @@
+import Foundation
+import CoreLocation
+import Combine
+
+class MapViewModel: NSObject, CLLocationManagerDelegate {
+    var locationSubject = CurrentValueSubject<LocationData?, Never>(nil)
+    private let locationManager = CLLocationManager()
+
+    override init() {
+        super.init()
+        locationManager.delegate = self
+    }
+
+    func requestLocationAuthorization() {
+        locationManager.requestWhenInUseAuthorization()
+    }
+
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            requestLocationAuthorization()
+        case .restricted, .denied:
+            break
+        case .authorizedAlways, .authorizedWhenInUse:
+            manager.startUpdatingLocation()
+            break
+        default:
+            break
+        }
+    }
