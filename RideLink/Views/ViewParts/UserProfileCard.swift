0d95c3b58af2f46e692aa4e4d76d5330627a8621
Sawaki Shuto <swkshuto0208@icloud.com>
2024-05-05 22:41:49 +0900

    追加: userProfileCardを作成するためのファイル追加

---
+++ RideLink/Views/ViewParts/UserProfileCard.swift
@@ -0,0 +1,18 @@
+//
+//  UserProfileCard.swift
+//  RideLink
+//
+//  Created by 澤木柊斗 on 2024/05/05.
+//
+
+import SwiftUI
+
+struct UserProfileCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserProfileCard()
+}
