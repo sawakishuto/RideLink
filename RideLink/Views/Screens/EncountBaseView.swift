8faabb5984caa17cbbc1de871350820a15cdf1fd
Sawaki Shuto <swkshuto0208@icloud.com>
2024-05-06 01:59:52 +0900

    変更: 別のViewにお願いするので削除

--- RideLink/Views/Screens/EncountBaseView.swift
+++ RideLink/Views/Screens/EncountBaseView.swift
@@ -10,17 +10,13 @@ import SwiftUI
 struct EncountBaseView: View {
     var body: some View {
         VStack(spacing: 20) {
-                Text("今日はこんなライダーとすれ違いました！")
-                    .font(.system(size: 20))
-                    .fontWeight(.bold)
                 CircleImage()
                 UserProfileCard()
                     .scaleEffect(0.9)
         }
         .padding(.top, 20)
         .frame( maxWidth: .infinity,  maxHeight: .infinity, alignment: .center)
-        .background(Color(hex: "F8F8F8"))
-        .ignoresSafeArea()
+
     }
 }
 
