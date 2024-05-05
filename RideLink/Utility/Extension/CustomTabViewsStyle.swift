4245745d9e3b96aa44607116f3350d04a2d7dbd6
Sawaki Shuto <swkshuto0208@icloud.com>
2024-05-05 22:14:16 +0900

    修正: padding等追加

--- RideLink/Utility/Extension/CustomTabViewsStyle.swift
+++ RideLink/Utility/Extension/CustomTabViewsStyle.swift
@@ -19,20 +19,20 @@ struct CustomTabViewsStyle: View {
                         currentTab = tab
                         AudioServicesPlaySystemSound(1519)
                     }, label: {
-                        ZStack() {
-                            Image(systemName: tab.synbolName())
-                                .renderingMode(.template)
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 30, height: 30)
-                                .frame(maxWidth: .infinity)
-                                .foregroundColor(currentTab == tab ? .green : .gray)
-                        }
+                        Image(systemName: tab.synbolName())
+                            .renderingMode(.template)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 30, height: 30)
+                            .frame(maxWidth: .infinity)
+                            .foregroundColor(currentTab == tab ? .green : .gray)
                     })
                 }
             }
             .frame(width: g.size.width)
         })
         .frame(height: 30)
+        .padding(.bottom, 10)
+        .padding([.horizontal, .top])
     }
 }
