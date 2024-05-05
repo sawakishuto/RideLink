b2b81c03b88a8fcf869427965d50e579292b5bd2
Sawaki Shuto <swkshuto0208@icloud.com>
2024-05-05 21:47:02 +0900

    修正: カスタムタブを適応

--- RideLink/Views/Screens/TabViews.swift
+++ RideLink/Views/Screens/TabViews.swift
@@ -10,22 +10,27 @@ import SwiftUI
 
 struct TabViews: View {
     init() {
-        UITabBar.appearance().unselectedItemTintColor = .black
-
-        UITabBar.appearance().backgroundColor = .white
+        UITabBar.appearance().isHidden = true
     }
+    @State var currentTab: Tab =  .map
     var body: some View {
-        TabView {
-            MapView()
-                .tabItem {
-                    Image(systemName: "map")
-                }
-            ContentView()
-                .tabItem {
-                    Image(systemName: "figure.stand.line.dotted.figure.stand")
-                }
+        VStack(spacing: 0) {
+            TabView(selection: $currentTab) {
+                MapView()
+                    .tag(Tab.map)
+
+                ContentView()
+                    .tag(Tab.encounts)
+
+                Text("Frired")
+                    .tag(Tab.friends)
+
+                Text("settings")
+                    .tag(Tab.settings)
+            }
+            Divider()
+            CustomTabViewsStyle(currentTab: $currentTab)
         }
-        .accentColor(.green)
     }
 }
 
