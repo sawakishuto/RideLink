ea619178f6ddb4ce3a13500f4cecc0a9fa140d2e
Sawaki Shuto <swkshuto0208@icloud.com>
2024-05-10 04:58:10 +0900

    変更: 写真はURLを使用するのでAsyncImageに変更

--- RideLink/Views/ViewParts/CircleImage.swift
+++ RideLink/Views/ViewParts/CircleImage.swift
@@ -8,16 +8,25 @@
 import SwiftUI
 
 struct CircleImage: View {
+    let ImageURL: String
+
     var body: some View {
-        Image("TestBikeImage")
-            .resizable()
-            .scaledToFit()
-            .frame(width: 250)
-            .cornerRadius(500)
-            .shadow(color: .gray, radius: 4, x: 2, y: 5)
+        AsyncImage(url: URL(string: ImageURL)!) { image in
+            image
+                .resizable()
+                .scaledToFit()
+                .frame(width: 250)
+                .cornerRadius(500)
+                .shadow(color: .gray, radius: 4, x: 2, y: 5)
+        } placeholder: {
+            ProgressView()
+                .frame(width: 250)
+                .cornerRadius(500)
+                .shadow(color: .gray, radius: 4, x: 2, y: 5)
+        }
     }
 }
 
 #Preview {
-    CircleImage()
+    CircleImage(ImageURL: "https://www.irasutoya.com/2013/05/blog-post_7246.html")
 }
