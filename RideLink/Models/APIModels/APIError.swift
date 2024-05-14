300058eea3c97ae96dc142554a4388b8eec0d49c
Sawaki Shuto <swkshuto0208@icloud.com>
2024-05-15 03:42:08 +0900

    add: 必要なenumを作成

---
+++ RideLink/Models/APIModels/APIError.swift
@@ -0,0 +1,8 @@
+//
+//  APIError.swift
+//  RideLink
+//
+//  Created by 澤木柊斗 on 2024/05/15.
+//
+
+import Foundation
---
+++ RideLink/Models/APIModels/HTTPMethods.swift
@@ -0,0 +1,15 @@
+//
+//  EndPoint.swift
+//  RideLink
+//
+//  Created by 澤木柊斗 on 2024/05/15.
+//
+
+import Foundation
+
+
+enum HTTPMethod {
+    case post
+    case get
+    case patch
+}
