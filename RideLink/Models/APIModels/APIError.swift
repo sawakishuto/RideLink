228e667451cab4b83a6b676c464c4f286975afdc
Sawaki Shuto <swkshuto0208@icloud.com>
2024-05-15 03:42:38 +0900

    feat: Errorハンドリングに使うenumを作成

--- RideLink/Models/APIModels/APIError.swift
+++ RideLink/Models/APIModels/APIError.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+enum APIError: Error {
+    case invalidUrl
+    case networkError
+    case unknown
+
+    var title: String {
+        switch self {
+
+        case .invalidUrl:
+            return "URLが無効です."
+        case .networkError:
+            return "ネットワークエラー"
+        case .unknown:
+            return "不明なエラー"
+        }
+
+        var description: String {
+            switch self {
+
+            case .invalidUrl:
+                return "URLが無効です。"
+            case .networkError:
+                return "接続環境の良いところでもう一度お試しください。"
+            case .unknown:
+                return "不明なエラーです。"
+            }
+        }
+    }
+}
