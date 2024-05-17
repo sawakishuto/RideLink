d36246d755ea855d183b7c8b6ea939ae15a10a48
aquaman1182 <hal.bandotakumi@gmail.com>
2024-05-17 17:30:28 +0900

    LoginView & 再利用性のあるパーツの作成

---
+++ RideLink/Views/Screens/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  RideLink
+//
+//  Created by 拓実 on 2024/05/09.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ RideLink/Views/ViewParts/AppButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  AppButtonView.swift
+//  RideLink
+//
+//  Created by 拓実 on 2024/05/09.
+//
+
+import SwiftUI
+
+struct AppButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppButtonView()
+}
