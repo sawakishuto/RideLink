7fc205467d338e01b28d5319caefcdd781838097
Sawaki Shuto <swkshuto0208@icloud.com>
2024-05-25 02:47:11 +0900

    変更: UserRepositoryProtocolを作成

--- RideLink/Repository/EncounterRepositoryProtocol.swift
+++ RideLink/Repository/EncounterRepositoryProtocol.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import Combine
+
+
+protocol EncounterRepositoryProtocol: AnyObject {
+
+    func sendFriendReqest(ownUid: String, toUid: String) -> AnyPublisher<Bool, Error>
+
+    func receptionFriendReqest() -> AnyPublisher<FriendInfoModel, Error>
+
+    func acceptFriendRequest(ownUid: String, toUid: String) -> AnyPublisher<Bool, Error>
+//    誰とすれ違ったかを取得するメソッド
+    func getEncountInfo() -> AnyPublisher<[EncountInfoModel], Error>
+//   自分の位置をポストするメソッド
+    func postUserLocation(userLocInfo: UserLocationInfoModel) -> AnyPublisher<Bool, Error>
+}
