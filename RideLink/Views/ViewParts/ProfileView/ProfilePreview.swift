47be78fe6c609282024cd0776f244f0d918564bd
kaka0913 <kabuyuu0913@gmail.com>
2024-05-28 01:55:30 +0900

    画像の更新機能実装

--- RideLink/Views/ViewParts/ProfileView/ProfilePreview.swift
+++ RideLink/Views/ViewParts/ProfileView/ProfilePreview.swift
@@ -9,22 +9,25 @@ import SwiftUI
 
 struct ProfilePreView: View {
     @Binding var profileData: UserProfileModel
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
 
     var body: some View {
         HStack {
-            ZStack(alignment: .bottomTrailing) {
-                Button(action: {
-                }) {
+            Button(action: {
+                self.showingImagePicker = true
+            }) {
+                ZStack(alignment: .bottomTrailing) {
                     Image(systemName: "camera.circle.fill")
                         .resizable()
                         .frame(width: 28, height: 28)
                         .foregroundColor(.gray)
                         .offset(x: -75, y: -75)
+                    Image(uiImage: inputImage ?? UIImage(named: profileData.profileIcon)!)
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .clipShape(Circle())
                 }
-                Image("kabuBike")//Todo修正
-                    .resizable()
-                    .frame(width: 100, height: 100)
-                    .clipShape(Circle())
             }
             .padding(.trailing, 20)
             VStack(alignment: .leading) {
@@ -32,7 +35,7 @@ struct ProfilePreView: View {
                     .font(.headline)
                 Spacer().frame(height: 10)
                 HStack {
-                    Image("bikeicon")//Todo修正
+                    Image("bikeicon")
                         .resizable()
                         .frame(width: 35, height: 21)
                     Text(profileData.bikeName)
@@ -45,9 +48,17 @@ struct ProfilePreView: View {
             Spacer()
         }
         .padding([.top, .leading, .trailing])
+        .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
+            ImagePicker(image: $inputImage)
+        }
         Rectangle()
             .fill(Color.gray.opacity(0.5))
             .frame(height: 3)
             .shadow(color: .gray, radius: 5, x: 0, y: 5)
+    }
+    
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        // 画像をアップロードするためのAPIを呼び出したり、画像をローカルに保存したりするコードをここに書く
     }
 }
