f27c3082989e4394419f930e98c8431367ca703a
Sawaki Shuto <swkshuto0208@icloud.com>
2024-06-08 13:42:53 +0900

    変更: 仮のものを追加

--- RideLink/Repository/Encount/EncounterRepository.swift
+++ RideLink/Repository/Encount/EncounterRepository.swift
@@ -10,3 +10,15 @@ import Combine
 import Alamofire
 
     let apiClient = APIClient.shared
+
+    func sendFriendReqest(toUid: String) -> AnyPublisher<Bool, Error> {
+        let subject = CurrentValueSubject<Bool, Error>(true)
+        subject.send(true)
+        return subject.eraseToAnyPublisher()
+    }
+
+    func receptionFriendReqest() -> AnyPublisher<FriendInfoModel, Error> {
+        let subject = CurrentValueSubject<FriendInfoModel, Error>(FriendInfoModel(id: "11", isOnline: true, profile: UserProfileModel(userName: "a", bikeName: "a", profileIcon: "x", touringcomment: nil)))
+        subject.send((FriendInfoModel(id: "11", isOnline: true, profile: UserProfileModel(userName: "a", bikeName: "a", profileIcon: "x", touringcomment: nil))))
+        return subject.eraseToAnyPublisher()
+    }
