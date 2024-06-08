62385a797ac7ad62aea4c599fa547285bf28ba5e
Sawaki Shuto <swkshuto0208@icloud.com>
2024-06-08 13:44:56 +0900

    トークンを取得するメソッドを追加

--- RideLink/Service/APIClient.swift
+++ RideLink/Service/APIClient.swift
@@ -13,9 +13,32 @@ import FirebaseAuth
 final class APIClient {
 
     static let shared = APIClient()
+    let auth = Auth.auth()
 
     private let baseUrl = "https://pokeapi.co/api/v2/pokemon/"
 
+
+
+    func getUserToken() -> Future <String, Error> {
+        return Future { promise in
+            guard let user = Auth.auth().currentUser else {
+                let error = NSError(domain: "com.example.app", code: 0, userInfo: [NSLocalizedDescriptionKey: "No user is signed in"])
+                promise(.failure(error))
+                return
+            }
+
+            user.getIDToken { token, error in
+                if let error = error {
+                    promise(.failure(error))
+                } else if let token = token {
+                    promise(.success(token))
+                } else {
+                    let error = NSError(domain: "com.example.app", code: 0, userInfo: [NSLocalizedDescriptionKey: "Failed to get ID token"])
+                    promise(.failure(error))
+                }
+            }
+        }
+    }
     // データを取得するメソッド  ジェネリクスで指定してるから柔軟に使えるはずだよ
     func fetchData<T: Decodable>(endPoint: paths.RawValue, params: Parameters, type: T.Type,headers: HTTPHeaders) -> AnyPublisher<T, Error> {
         return Deferred {
