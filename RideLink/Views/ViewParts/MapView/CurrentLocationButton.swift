dea85045229887d388519312377baa20f8ec8b08
Sawaki Shuto <swkshuto0208@icloud.com>
2024-06-10 03:44:42 +0900

    変更: 初期設定

--- RideLink/Views/ViewParts/MapView/CurrentLocationButton.swift
+++ RideLink/Views/ViewParts/MapView/CurrentLocationButton.swift
@@ -7,3 +7,13 @@
 
 import Foundation
 import UIKit
+
+final class CurrentLocationButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpButton()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
