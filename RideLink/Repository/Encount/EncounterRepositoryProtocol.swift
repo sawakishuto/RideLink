233585257f2a38ca479583e378d2165d0d07a0e5
Sawaki Shuto <swkshuto0208@icloud.com>
2024-06-13 01:36:06 +0900

    こちらもコメントアウト

--- RideLink/Repository/Encount/EncounterRepositoryProtocol.swift
+++ RideLink/Repository/Encount/EncounterRepositoryProtocol.swift
@@ -13,7 +13,7 @@ protocol EncounterRepositoryProtocol: AnyObject {
 
     func sendFriendReqest(toUid: String) -> AnyPublisher<Bool, Error>
 
-    func receptionFriendReqest() -> AnyPublisher<FriendInfoModel, Error>
+//    func receptionFriendReqest() -> AnyPublisher<FriendInfoModel, Error>
 
     func acceptFriendRequest(toUid: String) -> AnyPublisher<Bool, Error>
 //    誰とすれ違ったかを取得するメソッド
