44dc8ea55bd8efc53a4b4ed9563aa2edafdd025a
Sawaki Shuto <swkshuto0208@icloud.com>
2024-06-13 01:36:35 +0900

    変更: nilにした

--- RideLink/ViewModels/EncountViewModel.swift
+++ RideLink/ViewModels/EncountViewModel.swift
@@ -14,7 +14,7 @@ final class EncountViewModel: ObservableObject {
             userInfo: UserProfileModel(
                 userName: "しゅうと",
                 bikeName: "DSC400",
-                profileIcon: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhmLEQFa1-bwtYr1Pf2_TtiH5jSyY-hLW72Oi8ahq-RSqQlujO4TPvXz4vIugZIXampL_yGk1ReNEWTOPtEQfSQ3nSS0GTPkYJX4xCEhlE-ETT4yqW7CqLYv5EDdc4UjIMe3WIc0OittEwC/s800/bike_helmet_man.png",
+                profileIcon: nil,
                 touringcomment: "みんなよろしくね"
             ),
             touringInfo: TouringInfoModel(
@@ -28,7 +28,7 @@ final class EncountViewModel: ObservableObject {
             userInfo: UserProfileModel(
                 userName: "しゅうと",
                 bikeName: "DSC400",
-                profileIcon: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgIkaA2NYA1MjyDjhbiHLEqq8kq9dXEZ3lJM27gS6rbqjQuWZgnCeuCspZY4s64MangZ1SosRQuTARIDU23ycehzVIipHlMCmXYmo49-V87XRNPudMyfd5SBJG0s662Pm0Jr4rej5FO7bE9/s800/bike_offroad_motocross.png",
+                profileIcon: nil,
                 touringcomment: "みんなよろしくね"
             ),
             touringInfo: TouringInfoModel(
@@ -42,7 +42,7 @@ final class EncountViewModel: ObservableObject {
             userInfo: UserProfileModel(
                 userName: "しゅうと",
                 bikeName: "DSC400",
-                profileIcon: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjJP4jx3kZeCRfKzX0IsTB8_kce5gLi7raiFba58VKyK3F86xFAdiOXLA9sqWJVTKv-APSmtsJet_zrMjBVLmYbbTT_yTyUdUHIxNTr_ePtMwXxA1kGHcRC7vm3UHwjiMbeM2jdmuDd0eft/s800/bike_american_choppers_man.png",
+                profileIcon: nil,
                 touringcomment: "みんなよろしくね"
             ),
             touringInfo: TouringInfoModel(
@@ -56,7 +56,7 @@ final class EncountViewModel: ObservableObject {
             userInfo: UserProfileModel(
                 userName: "しゅうと",
                 bikeName: "DSC400",
-                profileIcon: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh2d2lt0_uYp9zWhfiDHpqrSKLnKgfvmPdsh0MkEo6dfqZTF52It_O98C6SsqH87x0KWGtJNdSWqrAFVNn6T9n8jszhJF3B62-ycrAbAG4wSCEckDf4uI_qZwDUdt8cfhU4xpTkuzgFzLU/s800/delivery_bike.png",
+                profileIcon: nil,
                 touringcomment: "みんなよろしくね"
             ),
             touringInfo: TouringInfoModel(
