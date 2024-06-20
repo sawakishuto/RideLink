a5fb6209e4348bd82c4fa86e91d4a0cf71459659
Sawaki Shuto <swkshuto0208@icloud.com>
2024-06-21 03:53:21 +0900

    追加: イベント通知を購読して、その対象になる関数を追加

--- RideLink/ViewModels/EncountViewModel.swift
+++ RideLink/ViewModels/EncountViewModel.swift
@@ -7,6 +7,32 @@ final class EncountViewModel: ObservableObject {
 
     init(repository: EncounterRepositoryProtocol) {
         self.encountRepository = repository
+        NotificationCenter.default.addObserver(self, selector: #selector(handleNotification), name: Notification.Name("RecieveNotification"), object: nil)
+    }
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+
+    @objc func handleNotification(_ notification: Notification) {
+        guard let userInfo = notification.userInfo else {return}
+        let encounter = userInfo["encounter"] as! Int
+        encountRepository.getEncountInfo()
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] in
+                guard let self = self else {return}
+                switch $0 {
+                case .finished:
+                    print("終了します")
+                    return
+                case .failure(let error):
+                    print(error)
+                    return
+                }
+            } receiveValue: { encountInfo in
+                self.encountInfos = encountInfo
+            }
+            .store(in: &cancellables)
+
     }
 
     @Published var encountInfos: [EncountInfoModel] = [
