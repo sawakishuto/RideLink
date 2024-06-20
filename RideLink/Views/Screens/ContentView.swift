67e09c6a9ce1ceb05065588eb569183776701cfa
Sawaki Shuto <swkshuto0208@icloud.com>
2024-06-21 03:48:36 +0900

    追加: payloadを解析した結果によって出汁引きできるようにした

--- RideLink/Views/Screens/ContentView.swift
+++ RideLink/Views/Screens/ContentView.swift
@@ -8,18 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var notificationPayload: [AnyHashable: Any]?
+    @ObservedObject var vm = ContentViewModel()
+
 
     var body: some View {
         GeometryReader { geometory in
 
             ZStack(alignment: .center){
 
-                bannerNotification(encountCount: 10)
-                    .position(
-                        x: geometory.size.width * 0.56,
-                        y: geometory.size.height * 0.6
-                    )
-                    .zIndex(100)
+                if vm.isRecieveNotification {
+                    bannerNotification(encountCount: vm.count, isRecieveNotification: $vm.isRecieveNotification)
+                        .position(
+                            x: geometory.size.width * 0.56,
+                            y: geometory.size.height * 0.6
+                        )
+                        .zIndex(100)
+                }
 
                 MapView()
             }
