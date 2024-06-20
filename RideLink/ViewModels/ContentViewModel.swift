1ce4e1ecdfdc8658aa528aa1c39c338de697f86b
Sawaki Shuto <swkshuto0208@icloud.com>
2024-06-21 03:49:00 +0900

    追加: 変数追加とNotificationCenterを追加

--- RideLink/ViewModels/ContentViewModel.swift
+++ RideLink/ViewModels/ContentViewModel.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import UserNotifications
+
+final class ContentViewModel: ObservableObject {
+    @Published var isRecieveNotification: Bool = false
+    @Published var count: Int = 0
+
+    init() {
+        NotificationCenter.default.addObserver(self, selector: #selector(handleNotification), name: Notification.Name("RecieveNotification"), object: nil)
+    }
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+
