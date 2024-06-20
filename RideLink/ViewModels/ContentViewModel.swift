013b55f235e4db2ec211b4e163840e73547b0bd3
Sawaki Shuto <swkshuto0208@icloud.com>
2024-06-21 03:50:58 +0900

    変更: 通知を投げる処理をViewModelへ変更(これはのちに別のクラスに分けた方がいいかもだ)

--- RideLink/ViewModels/ContentViewModel.swift
+++ RideLink/ViewModels/ContentViewModel.swift
@@ -27,3 +27,23 @@ final class ContentViewModel: ObservableObject {
         count = encounter
         sendLocalNotification(count: encounter)
     }
+
+    func sendLocalNotification(count: Int) {
+        let content = UNMutableNotificationContent()
+        content.title = "すれ違いがありました！"
+        content.body = "\(count)人とすれ違いました！👍"
+
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let request = UNNotificationRequest(identifier: "localNotification", content: content, trigger: trigger)
+
+        UNUserNotificationCenter.current().add(request) { (error) in
+            if let error = error {
+                print("通知の送信に失敗しました: \(error.localizedDescription)")
+            } else {
+                print("通知を送信しました")
+            }
+        }
+    }
+
+}
+
