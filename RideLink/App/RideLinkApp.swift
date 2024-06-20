e2753fd7dfea5329546f170658a7094c5cbe19bb
Sawaki Shuto <swkshuto0208@icloud.com>
2024-06-21 03:54:28 +0900

    変更: これはなくても通知を取得できるので良い

--- RideLink/App/RideLinkApp.swift
+++ RideLink/App/RideLinkApp.swift
@@ -46,7 +46,8 @@ class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDele
     }
 
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        completionHandler([.alert, .sound, .badge])
+
+    }
     }
 }
 
